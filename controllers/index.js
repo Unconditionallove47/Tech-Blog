349ef7a2c0d42609b58f426be673e8a665d4fcac
Kalif P <kalifpurce@gmail.com>
2022-06-10 16:48:59 -0700

    wrong camelcase

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ router.use("/api/blogs",blogRoutes)
 const commentRoutes = require("./api/commentRoutes");
 router.use("/api/comments",commentRoutes)
 
-const frontEnd = require("./frontendRoutes");
+const frontEnd = require("./frontEndRoutes");
 router.use("/",frontEnd)
 
 module.exports = router;
