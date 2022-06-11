45aa25dd476c05ef8bd9b4958bdd2cd03fe87352
Kalif P <kalifpurce@gmail.com>
2022-06-10 17:12:03 -0700

    seeds error fixed

--- seeds/index.js
+++ seeds/index.js
@@ -18,17 +18,17 @@ const users = [
 
 const blogs = [
     {
-        title:"my first blog",
+        name:"my first blog",
         body:"Welcome to my blog, im going to do this every day! Like share subscribe",
         UserId:1
     },
     {
-        title:"my final blog",
+        name:"my final blog",
         body:"I cant do this anymore, blogging every day is too hard.  It was a fun half week yall",
         UserId:1
     },
     {
-        title:"Cats: a review",
+        name:"Cats: a review",
         body:"I love cats I love every kind of cat.  I want to hug all them but you cant. Cant hug every cat......Cant hug every cat. ",
         UserId:2
     }
